941643007d0264a490b1d4642bdfe76bfaf1c19f
bss <baekss0409@gmail.com>
2020-10-17 17:09:52 +0900

    data-jpa 페이징 컨버터 사용

--- src/main/java/study/datajpa/controller/MemberController.java
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -2,11 +2,16 @@ package study.datajpa.controller;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.repository.MemberRepository;
 
@@ -28,9 +33,20 @@ public class MemberController {
 		return member.getUsername();
 	}
 	
+	//http://localhost:8080/members?page=3&size=5&sort=id,desc
+	@GetMapping("/members")
+	//http://localhost:8080/members 로 요청시 디폴트(@PageableDefault(로컬) 또는 yml(글로벌)로 설정)
+	public Page<MemberDto> list(@PageableDefault(size=5, sort="username", direction=Direction.ASC) Pageable pageable) {
+		Page<Member> page = memberRepository.findAll(pageable);
+		Page<MemberDto> map = page.map(MemberDto::new);
+		return map;
+	}
+	
 	@PostConstruct
 	public void init() {
-		Member m1 = new Member("감녕", 10, null);
-		memberRepository.save(m1);
+		for(int i=0 ; i<100 ; i++) {
+			Member m1 = new Member("감녕"+i, i, null);
+			memberRepository.save(m1);
+		}
 	}
 }
--- src/main/java/study/datajpa/dto/MemberDto.java
+++ src/main/java/study/datajpa/dto/MemberDto.java
@@ -1,11 +1,25 @@
 package study.datajpa.dto;
 
 import lombok.Data;
+import study.datajpa.entity.Member;
 
 @Data
 public class MemberDto {
 
-	private final Long id;
-	private final String username;
-	private final String teamName;
+	private Long id;
+	private String username;
+	private String teamName;
+	
+	public MemberDto(Member member) {
+		this.id = member.getId();
+		this.username = member.getUsername();
+		this.teamName = "재야";
+	}
+
+	public MemberDto(Long id, String username, String teamName) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.teamName = teamName;
+	}
 }
